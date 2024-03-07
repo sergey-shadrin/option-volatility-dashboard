d70404f61df4069ceca3a439d8400f693e84df83
sergey.shadrin <sergey.shadrin@ispringsolutions.com>
2024-03-07 23:44:11 +0300

    Рефакторинг js кода

--- src/templates/volatility_chart.js
+++ src/templates/volatility_chart.js
@@ -1,5 +1,3 @@
-g_chart = null;
-
 function requestChartData() {
     // Create a new XMLHttpRequest object
     var xhr = new XMLHttpRequest();
@@ -14,15 +12,7 @@ function requestChartData() {
         // Parse the JSON response
         var jsonResponse = JSON.parse(xhr.responseText);
 
-        console.log(jsonResponse);
-        if (g_chart == null)
-        {
-          initChart(jsonResponse);
-        }
-        else
-        {
-          updateChart(jsonResponse);
-        }
+        updateChart(jsonResponse);
       } else {
         // Handle errors
         console.error('Request failed with status code ' + xhr.status);
@@ -51,24 +41,15 @@ const verticalLinePlugin = {
       let pixelToLabelValueRatio = scaleX.width / (lastTickLabel - firstTickLabel);
       let linePosition = scaleX.left + (lineX - firstTickLabel) * pixelToLabelValueRatio;
 
-      console.log('line position: ' + linePosition);
-      console.log(scaleX.ticks[0]);
-      console.log(scaleX.ticks[scaleX.ticks.length - 1])
-      console.log(lineX);
       const meta = chart.getDatasetMeta(0); // first dataset is used to discover X coordinate of a point
       const data = meta.data;
-      console.log(data);
       return linePosition;
   },
 
   renderVerticalLine: function(chartInstance, lineX) {
       const lineLeftOffset = this.getLinePosition(chartInstance, lineX);
-      console.log(lineLeftOffset);
-      console.log(chartInstance.scales);
       const scaleY = chartInstance.scales.y;
-      console.log(scaleY);
       const context = chartInstance.ctx;
-      console.log(context);
 
       // render vertical line
       context.beginPath();
@@ -89,7 +70,6 @@ const verticalLinePlugin = {
       }
   }
 };
-Chart.register(verticalLinePlugin);
 
 function updateChart(chartData) {
     let labels = [];
@@ -122,43 +102,28 @@ function updateChart(chartData) {
     g_chart.update();
 }
 
-function initChart(chartData) {
-    let labels = [];
-    let stockVolatilities = [];
-    let callAskVolatilities = [];
-    let callBidVolatilities = [];
-    let callLastPriceVolatilities = [];
-    let putAskVolatilities = [];
-    let putBidVolatilities = [];
-    lastPrice = chartData['last_price'];
-    strikesData = chartData['strikes'];
-    strikesData.map(function(item) {
-        labels.push(item['strike']);
-        stockVolatilities.push(item['volatility']);
-        callAskVolatilities.push(item['call_ask_volatility']);
-        callBidVolatilities.push(item['call_bid_volatility']);
-        callLastPriceVolatilities.push(item['call_last_price_volatility']);
-        putAskVolatilities.push(item['put_ask_volatility']);
-        putBidVolatilities.push(item['put_bid_volatility']);
-    });
+function initChart() {
+    Chart.defaults.color = '#ffffff';
+    Chart.register(verticalLinePlugin);
+
     const pointRadius = 8;
     const pointHoverRadius = pointRadius + 1;
     const ctx = document.getElementById('volatilityChart');
 
-    g_chart = new Chart(ctx, {
+    return new Chart(ctx, {
       type: 'line',
       data: {
-        labels: labels,
+        labels: [],
         datasets: [{
           label: 'Volatility',
-          data: stockVolatilities,
+          data: [],
           fill: false,
           cubicInterpolationMode: 'monotone',
           tension: 0.2,
           borderColor: 'rgb(255, 0, 0)'
         }, {
           label: 'Call Ask',
-          data: callAskVolatilities,
+          data: [],
           fill: false,
           showLine: false,
           borderColor: 'rgb(255, 0, 0)',
@@ -173,7 +138,7 @@ function initChart(chartData) {
           }
         }, {
           label: 'Call Bid',
-          data: callBidVolatilities,
+          data: [],
           fill: false,
           showLine: false,
           borderColor: 'rgb(0, 160, 0)',
@@ -187,7 +152,7 @@ function initChart(chartData) {
           }
         }, {
           label: 'Put Ask',
-          data: putAskVolatilities,
+          data: [],
           fill: false,
           showLine: false,
           borderColor: 'rgb(255, 160, 160)',
@@ -202,7 +167,7 @@ function initChart(chartData) {
           }
         }, {
           label: 'Put Bid',
-          data: putBidVolatilities,
+          data: [],
           fill: false,
           showLine: false,
           borderColor: 'rgb(0, 255, 0)',
@@ -216,7 +181,7 @@ function initChart(chartData) {
           }
         }, {
           label: 'Call Last Price',
-          data: callLastPriceVolatilities,
+          data: [],
           fill: false,
           showLine: false,
           borderColor: 'rgb(255, 255, 0)',
@@ -232,7 +197,6 @@ function initChart(chartData) {
       },
       options: {
         animation: false,
-        color: '#ffffff',
         scales: {
             x: {
                 grid: {
@@ -247,12 +211,12 @@ function initChart(chartData) {
         },
         plugins: {
           'draw_vertical_line': {
-            lineX: lastPrice,
           }
         },
       },
     });
 }
 
+g_chart = initChart();
 requestChartData();
 setInterval(requestChartData, 3000);
