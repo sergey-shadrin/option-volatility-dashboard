a2d991daba6b024420b808861f1a4fda28dcb680
sergey.shadrin <sergey.shadrin@ispringsolutions.com>
2024-03-21 20:46:07 +0300

    Переделал способ получения данных об опционах из iss moex. Сначала извлекаются все доступные даты экспирации, а затем по ним происходит получение данных по опционам из таблицы опционов.

--- src/app/option_app.py
+++ src/app/option_app.py
@@ -111,11 +111,24 @@ class OptionApp:
         base_asset = self._init_base_asset_from_moex_api(base_asset_ticker)
         self._model.base_asset_repository.insert_base_asset(base_asset)
 
-        option_series_data = moex_api.get_option_series(base_asset.base_asset_code)
-        for option_series in option_series_data:
-            series_name = str(option_series['name'])
-            if series_name.startswith(base_asset.short_name):
-                self._populate_options_for_series(base_asset, option_series)
+        option_expirations = moex_api.get_option_expirations(base_asset_ticker)
+        for option_expiration_data in option_expirations:
+            series_type = option_expiration_data['series_type']
+            expiration_date = option_expiration_data['expiration_date']
+            self._populate_options_from_board(base_asset, series_type, expiration_date)
+
+    def _populate_options_from_board(self, base_asset: BaseAsset, series_type: str, expiration_date: str):
+        expiration_datetime = self._get_option_expiration_datetime(base_asset.base_asset_code, series_type,
+                                                                   expiration_date)
+        base_asset.add_expiration_datetime(expiration_datetime)
+
+        option_board_data = moex_api.get_option_board(base_asset.ticker, expiration_date)
+        for opt_type in option_board_data:
+            for option_data in option_board_data[opt_type]:
+                option_ticker = option_data['SECID']
+                strike = option_data['STRIKE']
+                option = Option(option_ticker, base_asset.ticker, expiration_datetime, strike, opt_type)
+                self._model.option_repository.insert_option(option)
 
     def _get_option_expiration_datetime(self, base_asset_code: str, series_type: str, expiration_date: str):
         expiration_datetime = expiration_date + 'T18:50:00'
@@ -123,27 +136,6 @@ class OptionApp:
         if base_asset_code in currency_base_asset_codes and series_type == option_series_type.QUARTER:
             expiration_datetime = expiration_date + 'T14:00:00'
         return datetime.fromisoformat(expiration_datetime)
-
-    def _populate_options_for_series(self, base_asset: BaseAsset, series_data: dict):
-        series_name = series_data['name']
-        series_type = series_data['series_type']
-        expiration_date = series_data['expiration_date']
-
-        expiration_datetime = self._get_option_expiration_datetime(base_asset.base_asset_code, series_type,
-                                                                   expiration_date)
-        base_asset.add_expiration_datetime(expiration_datetime)
-        option_list_data = moex_api.get_option_list_by_series(series_name)
-        for option_data in option_list_data:
-            self._populate_option_from_option_data(option_data, base_asset.ticker, expiration_datetime)
-
-    def _populate_option_from_option_data(self, option_data, base_asset_ticker, expiration_datetime):
-        is_traded = option_data['is_traded']
-        if is_traded:
-            ticker = option_data['secid']
-            strike = option_data['strike']
-            type = option_data['option_type']
-            option = Option(ticker, base_asset_ticker, expiration_datetime, strike, type)
-            self._model.option_repository.insert_option(option)
 
     def _init_base_asset_from_moex_api(self, base_asset_ticker):
         base_asset = BaseAsset(base_asset_ticker)
--- src/infrastructure/moex_api.py
+++ src/infrastructure/moex_api.py
@@ -1,12 +1,15 @@
 from urllib.parse import urlunparse
 from string import Template
 from infrastructure.api_utils import get_object_from_json_endpoint
+from model import option_type
 
 _SCHEME_HTTPS = 'https'
 _API_HOST = 'iss.moex.com'
 _OPTIONS_LIST_URL_TEMPLATE = Template('/iss/statistics/engines/futures/markets/options/series/$ticker/securities.json')
 _SECURITY_DESCRIPTION_URL_TEMPLATE = Template('/iss/securities/$ticker.json')
+_OPTION_EXPIRATIONS_URL = Template('/iss/statistics/engines/futures/markets/options/assets/$ticker.json')
 _OPTION_SERIES_URL = '/iss/statistics/engines/futures/markets/options/series.json'
+_OPTION_BOARD_URL_TEMPLATE = Template('/iss/statistics/engines/futures/markets/options/assets/$ticker/optionboard.json')
 
 
 def _make_absolute_url(relative_url: str) -> str:
@@ -26,6 +29,21 @@ def get_option_series(asset_code: str):
     url = _make_absolute_url(_OPTION_SERIES_URL)
     response = get_object_from_json_endpoint(url, params={'asset_code': asset_code})
     return _convert_moex_data_structure_to_list_of_dicts(response['series'])
+
+
+def get_option_expirations(base_asset_ticker: str):
+    url = _make_absolute_url(_OPTION_EXPIRATIONS_URL.substitute(ticker=base_asset_ticker))
+    response = get_object_from_json_endpoint(url)
+    return _convert_moex_data_structure_to_list_of_dicts(response['expirations'])
+
+
+def get_option_board(ticker: str, expiration_date: str):
+    url = _make_absolute_url(_OPTION_BOARD_URL_TEMPLATE.substitute(ticker=ticker))
+    response = get_object_from_json_endpoint(url, params={'expiration_date': expiration_date})
+    return {
+        option_type.CALL: _convert_moex_data_structure_to_list_of_dicts(response['call']),
+        option_type.PUT: _convert_moex_data_structure_to_list_of_dicts(response['put']),
+    }
 
 
 def get_option_list_by_series(option_series_ticker: str):
