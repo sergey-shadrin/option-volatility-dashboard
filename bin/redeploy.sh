49de0daa376c13a80a6bba5dba6955856a5a913a
sergey.shadrin <sergey.shadrin@ispringsolutions.com>
2024-03-22 23:58:08 +0300

    Сделал так, чтобы скрипт редеплоя можно было выполнять при вызове по абсолютному пути

--- bin/redeploy.sh
+++ bin/redeploy.sh
@@ -5,6 +5,10 @@ set -o nounset
 
 RUNTIME_IMAGE_TAG="shadrinsergey/option_volatility_dashboard:0.0.1"
 
+PROJECT_DIR="$(dirname "$(readlink -f "$0")")"
+
+cd "${PROJECT_DIR}"
+
 docker pull "${RUNTIME_IMAGE_TAG}" && \
   docker compose down && \
   docker compose up -d
