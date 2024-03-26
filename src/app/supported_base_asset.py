e636abe67ed8f95b17a5acd02ef795e1bac814d2
sergey.shadrin <sergey.shadrin@ispringsolutions.com>
2024-03-26 11:20:26 +0300

    Дополнил список поддерживаемых базовых активов

--- src/app/supported_base_asset.py
+++ src/app/supported_base_asset.py
@@ -11,4 +11,20 @@ MAP = {
         'strike_step': 0.05,
         'max_strikes_count': 11,
     },
+    'CRM4': {
+        'strike_step': 0.25,
+        'max_strikes_count': 11,
+    },
+    'BRK4': {
+        'strike_step': 1,
+        'max_strikes_count': 11,
+    },
+    'GZM4': {
+        'strike_step': 250,
+        'max_strikes_count': 11,
+    },
+    'SRM4': {
+        'strike_step': 250,
+        'max_strikes_count': 11,
+    },
 }
