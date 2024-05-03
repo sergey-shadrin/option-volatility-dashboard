17d8e550ffc82a4eae7d76a951edeab38ff55863
sergey.shadrin <sergey.shadrin@ispringsolutions.com>
2024-05-04 00:19:00 +0300

    В качестве компромисса упростил скрипты запуска и остановки окружения за счет дублирования значений тегов Docker-образов для runtime

--- bin/stop-dev.sh
+++ bin/stop-dev.sh
@@ -4,14 +4,5 @@ set -o errexit
 set -o nounset
 
 PROJECT_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
-# TODO: надо со всех компонентов собирать необходимые переменные окружения
-
-source "${PROJECT_DIR}/app/backend/build.env"
-BACKEND_RUNTIME_IMAGE_NAME=${RUNTIME_IMAGE_NAME}
-export BACKEND_RUNTIME_IMAGE_NAME
-
-source "${PROJECT_DIR}/app/frontend/build.env"
-FRONTEND_RUNTIME_IMAGE_NAME=${RUNTIME_IMAGE_NAME}
-export FRONTEND_RUNTIME_IMAGE_NAME
 
 cd "${PROJECT_DIR}" && docker-compose down --timeout 1
