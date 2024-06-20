f2a6753cabd980d19cc99d6ac81d86a98e855429
sergey.shadrin <sergey.shadrin@ispringsolutions.com>
2024-06-20 17:43:10 +0300

    Актуализировал список базовых активов - убрал истекающие, добавил актуальные

--- app/backend/src/app/supported_base_asset.py
+++ app/backend/src/app/supported_base_asset.py
@@ -1,15 +1,7 @@
 MAP = {
-    'RIM4': {
-        'strike_step': 2500,
-        'max_strikes_count': 7,
-    },
     'RIU4': {
         'strike_step': 2500,
         'max_strikes_count': 7,
-    },
-    'SiM4': {
-        'strike_step': 1000,
-        'max_strikes_count': 11,
     },
     'SiU4': {
         'strike_step': 1000,
@@ -19,11 +11,11 @@ MAP = {
         'strike_step': 0.25,
         'max_strikes_count': 11,
     },
-    'GZM4': {
+    'GZU4': {
         'strike_step': 250,
         'max_strikes_count': 11,
     },
-    'SRM4': {
+    'SRU4': {
         'strike_step': 250,
         'max_strikes_count': 11,
     },
