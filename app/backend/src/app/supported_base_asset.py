1673237cc4add7b62b86ba90ae33e733a7855648
s.shadrin <s.shadrin@tbank.ru>
2025-03-19 20:27:43 +0300

    Увеличил количество страйков для выборки у базовых активов. Добавил доп. конфигурацию для графана

--- app/backend/src/app/supported_base_asset.py
+++ app/backend/src/app/supported_base_asset.py
@@ -1,22 +1,22 @@
 MAP = {
     'RIM5': {
         'strike_step': 2500,
-        'max_strikes_count': 11,
+        'max_strikes_count': 15,
     },
     'SiM5': {
         'strike_step': 1000,
-        'max_strikes_count': 11,
+        'max_strikes_count': 15,
     },
     'CRM5': {
         'strike_step': 0.25,
-        'max_strikes_count': 11,
+        'max_strikes_count': 15,
     },
     'GZM5': {
         'strike_step': 250,
-        'max_strikes_count': 11,
+        'max_strikes_count': 15,
     },
     'SRM5': {
         'strike_step': 250,
-        'max_strikes_count': 11,
+        'max_strikes_count': 15,
     },
 }
