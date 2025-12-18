5e27056f728af26ba7392dbd217c05c4f8dd9f6a
Andy701 <90240370+andy7011@users.noreply.github.com>
2025-12-18 14:39:40 +0300

    Update supported_base_asset.py
    Поменял серии базовых активов

--- app/backend/src/app/supported_base_asset.py
+++ app/backend/src/app/supported_base_asset.py
@@ -1,12 +1,4 @@
 MAP = {
-    'RIZ5': {
-        'strike_step': 2500,
-        'max_strikes_count': 15,
-    },
-    'SiZ5': {
-        'strike_step': 1000,
-        'max_strikes_count': 15,
-    },
     'RIH6': {
         'strike_step': 2500,
         'max_strikes_count': 15,
@@ -15,11 +7,19 @@ MAP = {
         'strike_step': 1000,
         'max_strikes_count': 15,
     },
-    'SRZ5': {
-        'strike_step': 250,
+    'RIM6': {
+        'strike_step': 2500,
+        'max_strikes_count': 15,
+    },
+    'SiM6': {
+        'strike_step': 1000,
         'max_strikes_count': 15,
     },
     'SRH6': {
+        'strike_step': 250,
+        'max_strikes_count': 15,
+    },
+    'SRM6': {
         'strike_step': 250,
         'max_strikes_count': 15,
     },
