8f9d26376060c25b17875cb043fdcc627d13fbef
Andy701 <90240370+andy7011@users.noreply.github.com>
2025-12-29 15:18:58 +0300

    Update supported_base_asset.py
    16

--- app/backend/src/app/supported_base_asset.py
+++ app/backend/src/app/supported_base_asset.py
@@ -1,26 +1,26 @@
 MAP = {
     'RIH6': {
         'strike_step': 2500,
-        'max_strikes_count': 15,
+        'max_strikes_count': 16,
     },
     'SiH6': {
         'strike_step': 1000,
-        'max_strikes_count': 15,
+        'max_strikes_count': 16,
     },
     'RIM6': {
         'strike_step': 2500,
-        'max_strikes_count': 15,
+        'max_strikes_count': 16,
     },
     'SiM6': {
         'strike_step': 1000,
-        'max_strikes_count': 15,
+        'max_strikes_count': 16,
     },
     'SRH6': {
         'strike_step': 250,
-        'max_strikes_count': 15,
+        'max_strikes_count': 16,
     },
     'SRM6': {
         'strike_step': 250,
-        'max_strikes_count': 15,
+        'max_strikes_count': 16,
     },
 }
