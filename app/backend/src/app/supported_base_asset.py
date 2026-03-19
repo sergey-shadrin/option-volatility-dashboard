4dafe7d96ca8fe0f096d4e1ae6b8ad95028ed9ac
Andy701 <90240370+andy7011@users.noreply.github.com>
2026-03-19 19:08:44 +0300

    Update supported_base_asset.py
    Смена тикеров

--- app/backend/src/app/supported_base_asset.py
+++ app/backend/src/app/supported_base_asset.py
@@ -1,12 +1,4 @@
 MAP = {
-    'RIH6': {
-        'strike_step': 2500,
-        'max_strikes_count': 18,
-    },
-    'SiH6': {
-        'strike_step': 1000,
-        'max_strikes_count': 16,
-    },
     'RIM6': {
         'strike_step': 2500,
         'max_strikes_count': 18,
@@ -15,11 +7,19 @@ MAP = {
         'strike_step': 1000,
         'max_strikes_count': 16,
     },
-    'SRH6': {
-        'strike_step': 250,
+    'RIU6': {
+        'strike_step': 2500,
+        'max_strikes_count': 18,
+    },
+    'SiU6': {
+        'strike_step': 1000,
         'max_strikes_count': 16,
     },
     'SRM6': {
+        'strike_step': 250,
+        'max_strikes_count': 16,
+    },
+    'SRU6': {
         'strike_step': 250,
         'max_strikes_count': 16,
     },
