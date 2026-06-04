1b463bee1e07a3ab963acb843a0dcd6daad3f4b8
Sergey Shadrin <cps.shadrin@gmail.com>
2026-06-04 20:13:10 +0300

    Update implied_volatility.py
    При любых исключениях во время вычисления волатильности волатильность выставляется в значение None

--- app/backend/src/app/implied_volatility.py
+++ app/backend/src/app/implied_volatility.py
@@ -20,7 +20,7 @@ def get_iv_for_option_price(asset_price: int, option: Option, opt_price: int):
     tolerance = 10 ** -8
     try:
         iv = _implied_vol(opt_price, asset_price, strike_price, _RISK_FREE_INTEREST_RATE, time_to_maturity, tolerance, option.type)
-    except ZeroDivisionError:
+    except Exception:
         iv = None
 
     if not iv or math.isinf(iv) or math.isnan(iv):
