f085ceca24d9105e943f0c268ce46066ed011165
Andy701 <90240370+andy7011@users.noreply.github.com>
2026-06-19 08:01:36 +0300

    Update supported_base_asset.py
    Замена базовых активов

--- app/backend/src/app/supported_base_asset.py
+++ app/backend/src/app/supported_base_asset.py
@@ -1,12 +1,4 @@
 MAP = {
-    'RIM6': {
-        'strike_step': 2500,
-        'max_strikes_count': 18,
-    },
-    'SiM6': {
-        'strike_step': 1000,
-        'max_strikes_count': 16,
-    },
     'RIU6': {
         'strike_step': 2500,
         'max_strikes_count': 18,
@@ -15,11 +7,19 @@ MAP = {
         'strike_step': 1000,
         'max_strikes_count': 16,
     },
-    'SRM6': {
-        'strike_step': 250,
+    'RIZ6': {
+        'strike_step': 2500,
+        'max_strikes_count': 18,
+    },
+    'SiZ6': {
+        'strike_step': 1000,
         'max_strikes_count': 16,
     },
     'SRU6': {
+        'strike_step': 250,
+        'max_strikes_count': 16,
+    },
+    'SRZ6': {
         'strike_step': 250,
         'max_strikes_count': 16,
     },
